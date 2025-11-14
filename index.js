faf17239a2d42d96c15e31b59f5577b16a4aab42
AyushChakraborty <chakrabortyayush83@gmail.com>
2025-11-14 20:21:44 +0530

    feat: add bg ocean sound

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const heroImage = document.querySelector('.hero-image img')
+const waveImage = document.querySelector('#wave-sound')
+let isPlaying = false
+let audio = null
 
 window.addEventListener('scroll', () => {
     const scrollY = window.scrollY
@@ -6,9 +9,36 @@ window.addEventListener('scroll', () => {
     heroImage.style.transform = `translateY(${offset}px)`
 })
 
-window.addEventListener('load', () => {
-    const audio = new Audio('waves.mp3')
-    audio.loop = true 
-    audio.volume = 0.4
-    audio.play().catch(err => console.log("autoplay failed"))
+waveImage.addEventListener('click', () => {
+    if (!isPlaying) {
+        audio = new Audio('waves.mp3')
+        audio.loop = true 
+        audio.volume = 0.4
+        audio.play().catch(err => console.log("playback failed"))
+
+        waveImage.style.opacity = 0      //make the wave dissappear first
+        setTimeout(() => {
+            waveImage.src = "still_wave.png"
+            // waveImage.classList.add('still')
+            waveImage.style.opacity = 1    //make it appear back after change
+        }, 300)
+
+        isPlaying = true
+
+    }else {
+        if (audio) {
+            audio.pause()
+            audio.currentTime = 0
+            audio = null
+        }
+        waveImage.style.opacity = 0      
+        setTimeout(() => {
+            waveImage.src = "wave.png"
+            // waveImage.classList.remove('still')
+            waveImage.style.opacity = 1    
+        }, 300)
+
+        isPlaying = false
+    }
 })
+
